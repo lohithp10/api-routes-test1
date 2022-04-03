1496653397a90706c6358c09b1e796c4a3cbc3a5
lohithp10 <lohit.jiddimani@people10.com>
2022-04-03 21:20:44 +0530

    1 file modified

--- pages/api/flightschedule.js
+++ pages/api/flightschedule.js
@@ -2,11 +2,15 @@ export default function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(200).json({ message: 'flightschedule' });
     return;
+  } else {
+    const body = JSON.parse(req.body);
+    console.log(body);
+    if (body?.arrivalAirportScheduled) {
+      res.status(200).json({ message: 'flightschedule' });
+    } else {
+      res.status(405).json({ message: 'Bad Request' });
+    }
   }
-
-  const body = JSON.parse(req.body);
-
-  console.log(body);
 
   // the rest of your code
 }
